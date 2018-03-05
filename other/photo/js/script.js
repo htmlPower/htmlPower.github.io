5e9f206b8b4a872b728161b400ad59926ea2081b
htmlPower <kzyjxrf@yandex.ru>
2018-03-05 12:32:19 +0300

    9thCommit

--- other/photo/js/script.js
+++ other/photo/js/script.js
@@ -1,9 +1,17 @@
+
+$('.layout__side__slide').slick({
+  infinite: true,
+  dots: true,
+  arrows: false
+});
+
+
 var active = 'main';
   $(document).ready(function(){
-    $('.icon-link').click(function(e){
+    $('.nav__list-link').click(function(e){
         e.preventDefault();
         var id = $(this).attr('href');
-        $('.icon-link').removeClass('activ');
+        $('.nav__list-link').removeClass('activ');
         $(this).addClass('activ');     
         $('#'+active).addClass('none');
         $('#'+id).removeClass('none');
